ef016f51a2ab5bca565b29a6ae4c4dbeaed020a4
elyudwo <dudqk9696@naver.com>
2023-03-25 16:11:35 +0900

    refactor : 로컬 타임 수정

--- src/main/java/com/jojoIdu/book/freelecspringboot2webservice/web/dto/PostsListResponseDto.java
+++ src/main/java/com/jojoIdu/book/freelecspringboot2webservice/web/dto/PostsListResponseDto.java
@@ -10,13 +10,11 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Post entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
     }
 
 }
